8ea2bbbaf9c1c42a1a73c6f9f01117caa0f0a2bf
Faareh <faarehb@yahoo.com>
2023-11-13 12:59:00 -0500

    Used extract method to extract the common logic from the base and tax calculations into getBaseAmount() and getTaxAmount() methods for the ResidentialSite and LifelineSite classes.

--- src/Site.java
+++ src/Site.java
@@ -1,17 +1,11 @@
 class Site { }
 
 class ResidentialSite extends Site {
-    double getBillableAmount(){
-        double $base = _units * _rate;
-        double tax = base * Site.TAX_RATE;
-        return base + tax;
-    }
+    getBaseAmount();
+    getTaxAmount();
 }
 
 class LifelineSite extends Site{
-    double getBillableAmount(){
-        double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE * 0.2;
-        return base + tax;
-    }
+    getBaseAmount();
+    getTaxAmount();
 }
